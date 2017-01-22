b74e81649f6ff14f51d88c742d74cb69de1f5abe
zhangsuochao <zhangsuochao@baidu.com>
2017-01-22 11:14:54 +0800

    Base commit
    Change-Id: I973867166271b36abcefaf7031ba4360796d2075

---
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/hbase/HbaseGraph.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.hugegraph.hbase;
+
+import java.util.Iterator;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Transaction;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import com.baidu.hugegraph.structure.HugeGraph;
+
+/**
+ * Created by zhangsuochao on 17/1/16.
+ */
+public class HbaseGraph extends HugeGraph{
+    @Override
+    public Vertex addVertex(Object... keyValues) {
+        return null;
+    }
+
+    @Override
+    public <C extends GraphComputer> C compute(Class<C> graphComputerClass) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public GraphComputer compute() throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Iterator<Vertex> vertices(Object... vertexIds) {
+        return null;
+    }
+
+    @Override
+    public Iterator<Edge> edges(Object... edgeIds) {
+        return null;
+    }
+
+    @Override
+    public Transaction tx() {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    @Override
+    public Variables variables() {
+        return null;
+    }
+
+    @Override
+    public Configuration configuration() {
+        return null;
+    }
+}
---
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/structure/HugeGraph.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.hugegraph.structure;
+
+import org.apache.tinkerpop.gremlin.structure.Graph;
+
+/**
+ * Created by zhangsuochao on 17/1/16.
+ */
+public abstract class HugeGraph implements Graph{
+}
