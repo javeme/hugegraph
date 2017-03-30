2628d98d63c553fe0d368599af921fd7e0fde8a1
liningrui <liningrui@baidu.com>
2017-03-30 15:52:55 +0800

    repair cardinality, add Frequency enum object.
    Change-Id: I2913fe6d0ba8ab54b46ea51df88982ac99e03a75

--- hugegraph-core/src/main/java/com/baidu/hugegraph2/backend/query/HugeQuery.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph2/backend/query/HugeQuery.java
@@ -8,14 +8,16 @@ import java.util.Map;
  */
 public class HugeQuery implements Query {
 
-    private Map<String,Object> conditions;
+    private Map<String, Object> conditions;
     private int limit;
-    public HugeQuery(){
-        conditions = new LinkedHashMap<String,Object>();
+
+    public HugeQuery() {
+        conditions = new LinkedHashMap<String, Object>();
         limit = Integer.MAX_VALUE;
 
     }
-    public Map<String,Object> conditions(){
+
+    public Map<String, Object> conditions() {
         return conditions;
     }
 
@@ -26,7 +28,7 @@ public class HugeQuery implements Query {
 
     @Override
     public HugeQuery has(String key, Object value) {
-        this.conditions.put(key,value);
+        this.conditions.put(key, value);
         return this;
     }
 
--- hugegraph-core/src/main/java/com/baidu/hugegraph2/type/define/Cardinality.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph2/type/define/Cardinality.java
@@ -1,5 +1,7 @@
 package com.baidu.hugegraph2.type.define;
 
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
+
 /**
  * Created by jishilei on 17/3/18.
  */
@@ -9,13 +11,55 @@ public enum Cardinality {
     /**
      * Only a single value may be associated with the given key.
      */
-    SINGLE,
+    SINGLE(1),
+
+    /**
+     * Multiple values and duplicate values may be associated with the given key.
+     */
+    LIST(2),
 
     /**
      * Multiple but distinct values may be associated with the given key.
      */
-    MULTIPLE;
+    SET(3);
 
+    // HugeKeys define
+    private byte code = 0;
+
+    private Cardinality(int code) {
+        assert code < 256;
+        this.code = (byte) code;
+    }
+
+    public byte code() {
+        return this.code;
+    }
+
+    public VertexProperty.Cardinality convert() {
+        switch (this) {
+            case SINGLE:
+                return VertexProperty.Cardinality.single;
+            case LIST:
+                return VertexProperty.Cardinality.list;
+            case SET:
+                return VertexProperty.Cardinality.set;
+            default:
+                throw new AssertionError("Unrecognized cardinality: " + this);
+        }
+    }
+
+    public static Cardinality convert(VertexProperty.Cardinality cardinality) {
+        switch (cardinality) {
+            case single:
+                return SINGLE;
+            case list:
+                return LIST;
+            case set:
+                return SET;
+            default:
+                throw new AssertionError("Unrecognized cardinality: " + cardinality);
+        }
+    }
 
     public String schema() {
         // enum object -> string -> lowercase
