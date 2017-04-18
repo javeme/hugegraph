c5ce1e224c30566d85b4e49e70a4752c52eb0b17
Zhangmei Li <lizhangmei@baidu.com>
2017-04-18 17:09:14 +0800

    Clean up no-used code
    Change-Id: I096e47737118c86a7302cd7dc69939186c9cf5ba

---
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/backend/BackendException.java
@@ -0,0 +1,35 @@
+package com.baidu.hugegraph.backend;
+
+import com.baidu.hugegraph.HugeException;
+
+/**
+ * Created by jishilei on 17/3/19.
+ */
+public class BackendException extends HugeException {
+
+    private static final long serialVersionUID = -1947589125372576298L;
+
+    /**
+     * @param msg Exception message
+     */
+    public BackendException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * @param msg   Exception message
+     * @param cause Cause of the exception
+     */
+    public BackendException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    /**
+     * Constructs an exception with a generic message
+     *
+     * @param cause Cause of the exception
+     */
+    public BackendException(Throwable cause) {
+        this("Exception in backend.", cause);
+    }
+}
--- hugegraph-core/src/main/java/com/baidu/hugegraph2/process/strategy/HugeGraphStepStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.baidu.hugegraph2.process.strategy;
-
-/**
- * Created by jishilei on 2017/3/28.
- */
-public class HugeGraphStepStrategy {
-}
