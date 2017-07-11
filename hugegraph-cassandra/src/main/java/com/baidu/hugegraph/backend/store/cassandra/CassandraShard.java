2fa01eb9542ad6f1a0f14b5cad1e2a7134359c3c
Zhangmei Li <lizhangmei@baidu.com>
2017-07-11 18:35:40 +0800

    HugeGraph-410: improve code style for hugegraph-cassandra
    Change-Id: I25f4893291ad6ff70b9e51a11a3881e9eaed548d

--- hugegraph-cassandra/src/main/java/com/baidu/hugegraph/backend/store/cassandra/CassandraShard.java
+++ hugegraph-cassandra/src/main/java/com/baidu/hugegraph/backend/store/cassandra/CassandraShard.java
@@ -26,7 +26,7 @@ import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.dht.Token.TokenFactory;
 
 import com.baidu.hugegraph.backend.BackendException;
-import com.baidu.hugegraph.type.Split;
+import com.baidu.hugegraph.type.Shard;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.ResultSet;
@@ -34,9 +34,15 @@ import com.datastax.driver.core.Row;
 import com.datastax.driver.core.TokenRange;
 import com.google.common.collect.ImmutableMap;
 
-public class CassandraSplit {
+/**
+ * CassandraShard is used for cassandra scanning operations.
+ * Each shard represents a range of tokens for a node.
+ * Reading data from a given shard does not cross multiple nodes.
+ */
+public class CassandraShard {
 
-    public static long MIN_SPLIT_SIZE = 1024 * 1024; // 1M
+    // The minimal shard size should > 1M, to prevent too many number of shards
+    public static long MIN_SHARD_SIZE = 1024 * 1024;
 
     private CassandraSessionPool.Session session;
     private String keyspace;
@@ -44,9 +50,8 @@ public class CassandraSplit {
 
     private IPartitioner partitioner;
 
-    public CassandraSplit(CassandraSessionPool.Session session,
-            String keyspace,
-            String table) {
+    public CassandraShard(CassandraSessionPool.Session session,
+                          String keyspace, String table) {
         this.session = session;
         this.keyspace = keyspace;
         this.table = table;
@@ -59,34 +64,35 @@ public class CassandraSplit {
      * @param splitPartitions: expected partitions count per split
      * @param splitSize: expected size(bytes) per split,
      *        splitPartitions will be ignored if splitSize is passed
-     * @return a list of Split
+     * @return a list of Shard
      */
-    public List<Split> getSplits(long splitPartitions, long splitSize) {
-        // canonical ranges, split into pieces
-        // fetching the splits in parallel
-        ExecutorService executor = new ThreadPoolExecutor(0, 128, 60L,
-                TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-        List<Split> splits = new ArrayList<>();
+    public List<Shard> getSplits(long splitPartitions, long splitSize) {
+        // Canonical ranges, split into pieces, fetch the splits in parallel
+        ExecutorService executor = new ThreadPoolExecutor(
+                0, 128, 60L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        List<Shard> splits = new ArrayList<>();
 
         try {
-            List<Future<List<Split>>> futures = new ArrayList<>();
+            List<Future<List<Shard>>> futures = new ArrayList<>();
 
-            // canonical ranges and nodes holding replicas
+            // Canonical ranges and nodes holding replicas
             Map<TokenRange, Set<Host>> masterRangeNodes = getRangeMap();
 
             for (TokenRange range : masterRangeNodes.keySet()) {
-                // for each token range, pick a live owner and ask it to
+                // For each token range, pick a live owner and ask it to
                 // compute bite-sized splits
-                futures.add(executor.submit(new SplitCallable(range,
-                        splitPartitions, splitSize)));
+                futures.add(executor.submit(new SplitCallable(
+                            range, splitPartitions, splitSize)));
             }
 
-            // wait until we have all the results back
-            for (Future<List<Split>> future : futures) {
+            // Wait until we have all the results back
+            for (Future<List<Shard>> future : futures) {
                 try {
                     splits.addAll(future.get());
                 } catch (Exception e) {
-                    throw new BackendException("Can't get cassandra splits", e);
+                    throw new BackendException("Can't get cassandra shards", e);
                 }
             }
             assert splits.size() > masterRangeNodes.size();
@@ -98,39 +104,50 @@ public class CassandraSplit {
         return splits;
     }
 
-    // NOTE: maybe we don't need this method
-    public List<Split> getSplits(String start, String end,
-            int splitPartitions, int splitSize) {
+    /**
+     * Get splits of a table in specified range
+     * NOTE: maybe we don't need this method
+     * @param start, end: the specified range
+     * @param splitPartitions: expected partitions count per split
+     * @param splitSize: expected size(bytes) per split,
+     *        splitPartitions will be ignored if splitSize is passed
+     * @return a list of Shard
+     */
 
-        ExecutorService executor = new ThreadPoolExecutor(0, 128, 60L,
-                TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-        List<Split> splits = new ArrayList<>();
+    public List<Shard> getSplits(String start, String end,
+                                 int splitPartitions, int splitSize) {
+
+        ExecutorService executor = new ThreadPoolExecutor(
+                0, 128, 60L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        List<Shard> splits = new ArrayList<>();
 
         try {
-            List<Future<List<Split>>> futures = new ArrayList<>();
+            List<Future<List<Shard>>> futures = new ArrayList<>();
             TokenFactory tokenFactory = this.partitioner.getTokenFactory();
             TokenRange tokenRange = rangeToTokenRange(new Range<>(
-                    tokenFactory.fromString(start),
-                    tokenFactory.fromString(end)));
+                                    tokenFactory.fromString(start),
+                                    tokenFactory.fromString(end)));
 
-            // canonical ranges and nodes holding replicas
+            // Canonical ranges and nodes holding replicas
             Map<TokenRange, Set<Host>> masterRangeNodes = getRangeMap();
 
             for (TokenRange range : masterRangeNodes.keySet()) {
                 for (TokenRange r : range.intersectWith(tokenRange)) {
-                    // for each tokenRange, pick a live owner and ask it
+                    // For each tokenRange, pick a live owner and ask it
                     // to compute bite-sized splits
-                    futures.add(executor.submit(new SplitCallable(r,
-                            splitPartitions, splitSize)));
+                    futures.add(executor.submit(new SplitCallable(
+                                r, splitPartitions, splitSize)));
                 }
             }
 
-            // wait until we have all the results back
-            for (Future<List<Split>> future : futures) {
+            // Wait until we have all the results back
+            for (Future<List<Shard>> future : futures) {
                 try {
                     splits.addAll(future.get());
                 } catch (Exception e) {
-                    throw new BackendException("Can't get cassandra splits", e);
+                    throw new BackendException("Can't get cassandra shards", e);
                 }
             }
             assert splits.size() >= masterRangeNodes.size();
@@ -143,27 +160,24 @@ public class CassandraSplit {
     }
 
     private boolean isPartitionerOpp() {
-        return (this.partitioner instanceof OrderPreservingPartitioner
-                || this.partitioner instanceof ByteOrderedPartitioner);
+        return this.partitioner instanceof OrderPreservingPartitioner ||
+               this.partitioner instanceof ByteOrderedPartitioner;
     }
 
     private TokenRange rangeToTokenRange(Range<Token> range) {
         TokenFactory tokenFactory = this.partitioner.getTokenFactory();
         Metadata metadata = this.session.metadata();
         return metadata.newTokenRange(
-                metadata.newToken(tokenFactory.toString(range.left)),
-                metadata.newToken(tokenFactory.toString(range.right)));
+                        metadata.newToken(tokenFactory.toString(range.left)),
+                        metadata.newToken(tokenFactory.toString(range.right)));
     }
 
-    private Map<TokenRange, Long> getSubSplits(
-            TokenRange tokenRange,
-            long splitPartitions,
-            long splitSize) {
+    private Map<TokenRange, Long> getSubSplits(TokenRange tokenRange,
+                                               long splitPartitions,
+                                               long splitSize) {
         try {
-            return describeSplits(this.session,
-                    this.keyspace, this.table,
-                    splitPartitions, splitSize,
-                    tokenRange);
+            return describeSplits(this.session, this.keyspace, this.table,
+                                  splitPartitions, splitSize, tokenRange);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -183,17 +197,17 @@ public class CassandraSplit {
             long splitPartitions,
             long splitSize,
             TokenRange tokenRange) {
+
         String query = String.format(
-                "SELECT mean_partition_size, partitions_count FROM %s.%s "
-                + "WHERE keyspace_name = ? AND table_name = ? AND "
-                + "range_start = ? AND range_end = ?",
+                "SELECT mean_partition_size, partitions_count FROM %s.%s " +
+                "WHERE keyspace_name = ? AND table_name = ? AND " +
+                "range_start = ? AND range_end = ?",
                 SchemaConstants.SYSTEM_KEYSPACE_NAME,
                 SystemKeyspace.SIZE_ESTIMATES);
 
-        ResultSet resultSet = session.execute(query,
-                keyspace, table,
-                tokenRange.getStart().toString(),
-                tokenRange.getEnd().toString());
+        ResultSet resultSet = session.execute(query, keyspace, table,
+                                              tokenRange.getStart().toString(),
+                                              tokenRange.getEnd().toString());
         Row row = resultSet.one();
 
         long meanPartitionSize = 0;
@@ -204,9 +218,9 @@ public class CassandraSplit {
             meanPartitionSize = row.getLong("mean_partition_size");
             partitionsCount = row.getLong("partitions_count");
             assert splitSize <= 0 || splitSize >= 1024 * 1024;
-            splitCount = splitSize > 0
-                    ? (meanPartitionSize * partitionsCount / splitSize)
-                    : (partitionsCount / splitPartitions);
+            splitCount = splitSize > 0 ?
+                         (meanPartitionSize * partitionsCount / splitSize) :
+                         (partitionsCount / splitPartitions);
         }
 
         // If we have no data on this split or the size estimate is 0,
@@ -220,7 +234,7 @@ public class CassandraSplit {
         List<TokenRange> ranges = tokenRange.splitEvenly((int) splitCount);
         Map<TokenRange, Long> rangesWithLength = new HashMap<>();
         for (TokenRange range : ranges) {
-            // sub-range (with its partitions count per sub-range)
+            // Add a sub-range (with its partitions count per sub-range)
             rangesWithLength.put(range, partitionsCount / splitCount);
         }
         return rangesWithLength;
@@ -230,7 +244,7 @@ public class CassandraSplit {
      * Gets a token tokenRange and splits it up according to the suggested size
      * into input splits that Hugegraph can use.
      */
-    class SplitCallable implements Callable<List<Split>> {
+    class SplitCallable implements Callable<List<Shard>> {
 
         private final TokenRange tokenRange;
         private final long splitPartitions;
@@ -241,14 +255,14 @@ public class CassandraSplit {
             if (splitSize <= 0) {
                 if (splitPartitions <= 0) {
                     throw new IllegalArgumentException(String.format(
-                            "The split-partitions must be > 0, but got %s",
-                            splitPartitions));
+                              "The split-partitions must be > 0, but got %s",
+                              splitPartitions));
                 }
-            } else if (splitSize < MIN_SPLIT_SIZE) {
+            } else if (splitSize < MIN_SHARD_SIZE) {
                 // splitSize at least 1M if passed
                 throw new IllegalArgumentException(String.format(
-                        "The split-size must be >= %s bytes, but got %s",
-                        MIN_SPLIT_SIZE, splitSize));
+                          "The split-size must be >= %s bytes, but got %s",
+                          MIN_SHARD_SIZE, splitSize));
             }
 
             this.tokenRange = tokenRange;
@@ -257,22 +271,24 @@ public class CassandraSplit {
         }
 
         @Override
-        public List<Split> call() throws Exception {
-            ArrayList<Split> splits = new ArrayList<>();
+        public List<Shard> call() throws Exception {
+            ArrayList<Shard> splits = new ArrayList<>();
 
-            Map<TokenRange, Long> subSplits = getSubSplits(this.tokenRange,
-                    this.splitPartitions, this.splitSize);
+            Map<TokenRange, Long> subSplits = getSubSplits(
+                    this.tokenRange,
+                    this.splitPartitions,
+                    this.splitSize);
             for (Map.Entry<TokenRange, Long> entry : subSplits.entrySet()) {
                 List<TokenRange> ranges = entry.getKey().unwrap();
                 for (TokenRange subrange : ranges) {
-                    String start = isPartitionerOpp()
-                            ? subrange.getStart().toString().substring(2)
-                            : subrange.getStart().toString();
-                    String end = isPartitionerOpp()
-                            ? subrange.getEnd().toString().substring(2)
-                            : subrange.getEnd().toString();
+                    String start = !isPartitionerOpp() ?
+                                   subrange.getStart().toString() :
+                                   subrange.getStart().toString().substring(2);
+                    String end = !isPartitionerOpp() ?
+                                 subrange.getEnd().toString() :
+                                 subrange.getEnd().toString().substring(2);
                     long length = entry.getValue();
-                    splits.add(new Split(start, end, length));
+                    splits.add(new Shard(start, end, length));
                 }
             }
             return splits;
